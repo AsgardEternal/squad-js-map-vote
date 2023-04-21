10de6482c188af33a3e534c8c875c89b79773d3c
Skillet <kilerskyman@gmail.com>
2023-04-21 19:31:01 -0400

    fix rcon return

--- mapvote.js
+++ mapvote.js
@@ -1155,7 +1155,7 @@ layer
             }
         }
 
-        for (const lay in Layers._layers.values()){
+        for (const lay of Layers.layers){
             this.verbose(1, 'mapvote found layer: ', lay.layerid);
         }
         this.verbose(1, 'Layer list updated', Layers.layers.length, 'total layers');
